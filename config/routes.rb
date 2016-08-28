f4c3c2d2ddbce424e18e48958e7c7a7873599f0e
dzldebby <debby.lingzl@gmail.com>
2016-08-28 13:03:07 +0000

    updated profile page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :statuses
   root to: 'statuses#index'
+
+  get '/:id', to: 'profiles#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
